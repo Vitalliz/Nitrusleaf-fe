1f24064bd82e0ba871051b70dc016a392f308911
LuizWKM <luizricardowm@outlook.com>
2025-04-28 22:36:45 -0300

    :hammer:feat: Algumas mudanças na tela de inicio, melhorando a tela e os botões.

--- nitrus-leaf/src/components/Hero/Hero.js
+++ nitrus-leaf/src/components/Hero/Hero.js
@@ -7,9 +7,9 @@ function Hero() {
     <section className="hero">
       <h1>Diagnóstico de deficiência da sua mexerica em um piscar de olhos!</h1>
       <div className="auth-buttons">
-       <a href="/Login" className="auth-button"><button>Entrar</button></a>
-        <button className="auth-button-register">Cadastrar-se</button>
-        <button className="auth-button google">Entrar com o Google</button>
+       <button className="auth-button"><a href="/Login" className="links">Entrar</a></button>
+       <button className="auth-button-register"><a href="/Cadastro" className="links">Cadastrar-se</a></button>
+       <button className="auth-button google"><a href="/GoogleL" className="links">Entrar com o Google</a></button>
       </div>
     </section>
   );
